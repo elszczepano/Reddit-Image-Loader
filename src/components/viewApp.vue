79e106f0036b9f2522d7fab41da776756df58c35
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-18 16:45:42 +0100

    Prepare files to create components

---
+++ src/components/viewApp.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+
+</template>
+
+<script>
+export default {
+  name: 'viewApp',
+  data () {
+    return {
+      msg: 'Reddit json'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/view.scss";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import viewApp from '@/components/viewApp'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'viewApp',
+      component: viewApp
     }
   ]
 })
