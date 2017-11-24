cd95b6c16debc7ea0be8a5e11b3a29e72a977810
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-24 16:45:15 +0100

    Getting a subreddit JSON URL

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,38 @@
 <template lang="html">
-<div class="">
-  <Menu></Menu>
-  <Content></Content>
-</div>
+  <div class="container">
+    <div class="top-bar-menu">
+        <ul>
+          <li class="title">Reddit image loader</li>
+          <li>https://reddit.com/r/<input type="text" id='subreddit' placeholder="your-path" v-on:keyup.enter="getURL"></li>
+          <li>and hit ENTER!</li>
+        </ul>
+    </div>
+    <div class="photo-container">
+
+    </div>
+  </div>
 </template>
 
 <script>
-import Menu from '@/components/menu'
-import Content from '@/components/content'
 export default {
   name: 'Main',
-  components: { Menu, Content }
+  data () {
+    return {
+      msg: 'Reddit json'
+    }
+  },
+  methods: {
+    sendRequest: function (myUrl) {
+      console.log(MyUrl)
+    },
+    getURL: function () {
+      const field = document.querySelector('#subreddit').value
+      let url = `https://reddit.com/r/${field}.json`
+      sendRequest(url)
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="scss" src="../assets/scss/content.scss" scoped>
 </style>
