dba524c2f72370e5b8af41f6ad5eb21528f9be71
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-30 17:53:16 +0100

    Prepare component structure with error handling

---
+++ src/components/ErrorDisplay.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+<div class="error-box">
+  <div class="error-box-header">
+    <span>Notice!</span>
+    <span class="fa fa-times" aria-hidden="true"></span>
+  </div>
+  <div class="error-box-body">
+      <p>Example notice</p>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
