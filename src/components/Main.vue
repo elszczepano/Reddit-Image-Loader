da7086f896ce087e93c8292a1c33da3921d69952
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-11-30 17:22:46 +0100

    Add font-awesome icons and solve es-lint error

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,12 +2,12 @@
   <div class="container">
     <div class="top-bar-menu">
         <ul>
-          <li class="title">Reddit image loader</li>
+          <li class="title">Reddit image loader <span class="fa fa-reddit-alien" aria-hidden="true"></span></li>
           <li>https://www.reddit.com/r/<input v-model="subreddit" type="text" id='subreddit' placeholder="subreddit-name" @keyup.enter="getURL"></li>
           <li>and hit ENTER!</li>
         </ul>
     </div>
-    <h1>Your pictures</h1>
+    <h1>Your pictures <span class="fa fa-picture-o" aria-hidden="true"></span></h1>
     <div class="photo-container">
         <img v-bind:src="picture" class="photo" v-for="picture in pictures">
     </div>
@@ -28,7 +28,7 @@ export default {
     sendRequest (url) {
       fetch(url).then(response => response.json())
       .then(receivedData => receivedData['data']['children'])
-      .then(list => this.pictures = list.map(listItem => (listItem['data']['preview'] === undefined) ? listItem['data']['url'] : listItem['data']['preview']['images'][0]['source']['url']))
+      .then(list => { this.pictures = list.map(listItem => (listItem['data']['preview'] === undefined) ? listItem['data']['url'] : listItem['data']['preview']['images'][0]['source']['url']) })
       .catch(e => alert(e))
     },
     getURL () {
