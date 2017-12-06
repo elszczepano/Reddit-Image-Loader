cd37afd8ca81f832248de4402e00c9d17b0adbdf
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-06 19:22:01 +0100

    Disappearing notice has an animation

--- src/components/ErrorDisplay.vue
+++ src/components/ErrorDisplay.vue
@@ -1,18 +1,19 @@
 <template lang="html">
-<div class="error-box">
-  <header class="error-box-header">
-    <span>Notice <span class="fa fa-exclamation-triangle" aria-hidden="true"></span></span>
-  </header>
-  <div class="error-box-body">
-      <p>{{this.errorValue}}</p>
-  </div>
-  <hr>
-  <footer class="error-box-footer">
-    <button type="submit" name="button" @click="$emit('close')">close</button>
-  </footer>
-</div>
+  <transition name="slide-fade">
+    <div class="error-box">
+      <header class="error-box-header">
+        <span>Notice <span class="fa fa-exclamation-triangle" aria-hidden="true"></span></span>
+      </header>
+      <div class="error-box-body">
+          <p>{{this.errorValue}}</p>
+      </div>
+      <hr>
+      <footer class="error-box-footer">
+        <button type="submit" name="button" @click="$emit('close')">close</button>
+      </footer>
+    </div>
+  </transition>
 </template>
-
 <script>
 export default {
   name: 'ErrorDisplay',
