d1e18d5ecebe8c3a38c19bede748d1a3386166ea
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2017-12-13 17:40:37 +0100

    Add proper path to font awesome
    Add proper path to font awesome

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueResource from 'vue-resource'
+import 'font-awesome/scss/font-awesome.scss'
 
 Vue.use(VueResource)
 Vue.http.options.root = 'http://www.reddit.com/r/'
