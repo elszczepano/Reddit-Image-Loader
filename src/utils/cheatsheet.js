3b4ad6a765895d576a80f88b809307abc89fc22d
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-09-01 13:58:06 +0200

    Add cheatsheet util.

---
+++ src/utils/cheatsheet.js
@@ -0,0 +1,100 @@
+const cheatSheetShort = [
+	'funny',
+	'pics',
+	'aww',
+	'food',
+	'gifs',
+	'earthporn',
+	'space',
+	'creepy',
+	'memes',
+	'travel'
+];
+
+const cheatSheet = {
+	popular: [
+		'funny',
+		'memes',
+		'roastme',
+		'pics',
+		'murderedbywords',
+		'gaming',
+		'gifs',
+		'interestingasfuck',
+		'dankmemes',
+		'insanepeoplefacebook',
+		'trashy',
+		'choosingbeggars',
+		'earthporn',
+		'food',
+		'foodporn',
+		'art',
+		'space',
+		'wtf',
+		'photography'
+	],
+	cars: [
+		'porsche',
+		'audi',
+		'bmw',
+		'volkswagen',
+		'lamborghini',
+		'volvo',
+		'mercedes',
+		'amg',
+		'ford',
+		'mustang',
+		'toyota',
+		'koenigsegg',
+		'ferrari',
+		'bentley',
+		'bugatti'
+	],
+	animals: [
+		'aww',
+		'cats',
+		'cat',
+		'dog',
+		'snakes',
+		'spiders',
+		'cockatoos',
+		'doge',
+		'shiba',
+		'frenchbulldog',
+		'pitbull'
+	],
+	games: [
+		'classicwow',
+		'gta',
+		'minecraft',
+		'fortnite',
+		'pubg'
+	],
+	others: [
+		'facebook',
+		'twitter',
+		'instagram',
+		'gonewild',
+		'tinder',
+		'dataisbeautiful',
+		'instant_regret',
+		'pcmasterrace',
+		'starterpacks',
+		'freefolk',
+		'cringetopia',
+		'europe',
+		'nsfw',
+		'wow',
+		'sadcringe',
+		'tumblr',
+		'greentext',
+		'photoshopbattles',
+		'facepalm',
+		'tattoos',
+		'peopleofwalmart',
+		'lol',
+		'joker'
+	]
+};
+
+export { cheatSheet, cheatSheetShort };
