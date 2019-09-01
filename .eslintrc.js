8991f568b899ce6fbb985abd368004b2acb8980f
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-09-01 11:49:37 +0200

    Update linting and config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'semi': 'error'
   }
 }
