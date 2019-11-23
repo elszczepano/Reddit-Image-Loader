c9b52511ecf982c455d8ca46e96e67b08baf018a
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2019-11-23 13:22:57 +0100

    Replace infinite scrolling with pagination. Fix fetching images.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
 import Vue from 'vue';
-import InfiniteLoading from 'vue-infinite-loading';
 import App from './App';
 import router from './router';
 import 'font-awesome/scss/font-awesome.scss';
-
-Vue.use( InfiniteLoading, {
-	props: {
-		spinner: 'spiral'
-	},
-	slots: {
-		noResults: 'Provide a subreddit',
-		noMore: 'No more results found'
-	}
-} );
 
 Vue.config.productionTip = false;
 
